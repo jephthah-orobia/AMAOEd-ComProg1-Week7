d9e1216feb36f144c8908c5283a2501d239a68a4
jephthah-orobia <jephthah.orobia@gmail.com>
2024-12-26 13:02:40 -0600

    fix wrong hint on invalid input

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,7 @@ int main()
       cin.clear();
       cin.ignore(numeric_limits<streamsize>::max(), '\n');
       cout << endl
-           << "Invalid height. Try again [3-" << maxC << " integers only]: ";
+           << "Invalid height. Try again [2-" << maxC << " integers only]: ";
     }
   }
 
